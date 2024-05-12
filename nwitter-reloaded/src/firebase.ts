65b972a35cdb1b21d56ab6f12ad596ca474fafd2
jung2941 <ujin2941@naver.com>
2024-05-12 17:52:27 +0900

    3-1

--- nwitter-reloaded/src/firebase.ts
+++ nwitter-reloaded/src/firebase.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDtwlJFlDAJUJ0xE6z3a0Zzc9mObti6V4Y",
@@ -10,3 +11,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
