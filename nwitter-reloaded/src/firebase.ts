523ef6a9d172d5c241b44f6e5ceeadbbdfd4196e
jung2941 <ujin2941@naver.com>
2024-05-12 17:42:51 +0900

    nwitter

---
+++ nwitter-reloaded/src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDtwlJFlDAJUJ0xE6z3a0Zzc9mObti6V4Y",
+  authDomain: "nwitter-reloaded-2bdf6.firebaseapp.com",
+  projectId: "nwitter-reloaded-2bdf6",
+  storageBucket: "nwitter-reloaded-2bdf6.appspot.com",
+  messagingSenderId: "354077899730",
+  appId: "1:354077899730:web:7404dbf0d62fcd4070e60b"
+};
+
+const app = initializeApp(firebaseConfig);
